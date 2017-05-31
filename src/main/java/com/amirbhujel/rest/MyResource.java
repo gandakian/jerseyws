ff7b80013f92d93a761b41f773bd04a64bd6dcfd
gandakian <bhujel.amir@gmail.com>
2017-05-31 02:12:01 -0500

    Completed the implementation of Jersey WebService

---
+++ src/main/java/com/amirbhujel/rest/MyResource.java
@@ -0,0 +1,15 @@
+package com.amirbhujel.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+@Path("/myresource")
+public class MyResource {
+	
+	@GET
+	public String hello(){
+		
+		return "Hello Jersey";
+	}
+
+}
